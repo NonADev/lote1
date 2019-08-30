73c726f9d5baa268f20852a972d709653f8c8adc
nonadev <wesley.vieira.lima.1999@gmail.com>
2019-08-29 21:40:51 -0300

    a

--- Algoritmo_Java/src/lote_linear/Ex_25.java
+++ Algoritmo_Java/src/lote_linear/Ex_25.java
@@ -3,19 +3,21 @@ package lote_linear;
 public class Ex_25 {
 
 	public static void main(String[] args) {
-		int tInicial;
-		int tFinal;
-		int tPassado;
-		java.util.Scanner scanner = new java.util.Scanner(System.in);
-
-		tInicial = scanner.nextInt();
-		tFinal = scanner.nextInt();
-
-		tPassado = tFinal - tInicial;
-		if(tFinal<=tInicial) tPassado = 24 + tPassado;
-		
-		
-		System.out.println(String.format("O JOGO DUROU %d HORA(S)", tPassado));
-		scanner.close();		
+		java.util.Scanner s = new java.util.Scanner(System.in);
+		int start[] = { s.nextInt(), s.nextInt() }, end[] = { s.nextInt(), s.nextInt() };
+		int hours = 0, mins = 0;
+		do {
+			start[1]++;
+			if (start[1] == 60) {
+				start[1] = 0;
+				start[0]++;
+			}
+			if (start[0] == 24)
+				start[0] = 0;
+			hours++;
+		} while (!(start[0] == end[0] && start[1] == end[1]));
+		mins = hours % 60;
+		hours = hours / 60;
+		System.out.println("Hours: " + hours + " Mins: " + mins);
 	}
 }
